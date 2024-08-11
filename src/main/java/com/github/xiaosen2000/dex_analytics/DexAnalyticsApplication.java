7a20bbd7fc595388af172c0908eb41f5ee8c0623
Xiaosen Wang <xiaosen2@illinois.edu>
2024-08-11 21:47:14 +0800

    initial commit

---
+++ src/main/java/com/github/xiaosen2000/dex_analytics/DexAnalyticsApplication.java
@@ -0,0 +1,13 @@
+package com.github.xiaosen2000.dex_analytics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DexAnalyticsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DexAnalyticsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/xiaosen2000/dex_analytics/DexAnalyticsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.xiaosen2000.dex_analytics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DexAnalyticsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
